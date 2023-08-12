ca6bf334eeeaa4c67b52b9049825f9ab36652b34
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2023-08-12 11:19:11 +0900

    sample: modify permute sample

--- kobologs/00000000_sample/permute.rs
+++ kobologs/00000000_sample/permute.rs
@@ -21,3 +21,20 @@ fn main() {
     let result: Vec<String> = result.into_iter().collect();
     println!("{:?}", result);
 }
+
+// 0 0
+// 1 1
+// ['a', 'b', 'c']
+// 1 2
+// ['a', 'c', 'b']
+// 0 1
+// 1 1
+// ['b', 'a', 'c']
+// 1 2
+// ['b', 'c', 'a']
+// 0 2
+// 1 1
+// ['c', 'b', 'a']
+// 1 2
+// ['c', 'a', 'b']
+// acb
